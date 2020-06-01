ca2c30f0666fbac7c704e761054f2494fbbf8b48
simplemake <62598981+simplemake@users.noreply.github.com>
2020-06-01 10:08:02 +0530

    Add files via upload

---
+++ rearrange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import re
+
+def rearrange_name (name):
+    result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+    #The specific case if the string is empty.
+    if result is None:
+        return name
+    return "{} {}".format (result[2], result[1])
+
+
